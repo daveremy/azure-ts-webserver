b584518ad67da9ec1e89a60b40e30db65fae13c3
James Nugent <james@jen20.com>
2018-09-08 15:23:03 +0100

    Wait for Outputs to resolve before using as data source parameters

--- index.ts
+++ index.ts
@@ -1,37 +1,34 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
+import * as pulumi from "@pulumi/pulumi";
 import { Config } from "@pulumi/pulumi";
-import { ServicePrincipalPassword } from "@pulumi/azure/ad";
-
-const deploymentId = pulumi.getStack();
 
 // note: config name must be same as project name specified in pulumi.yaml
 const config = new Config("azure-test");
 const username = config.require("username");
 const password = config.require("password");
-const shortName = "aztest"
+const shortName = "aztest";
 
 const resourceGroup = new azure.core.ResourceGroup(shortName, {
-    location: config.require("location")
+    location: config.require("location"),
 });
 
 // network setup --------------------------------------------------------------
 const network = new azure.network.VirtualNetwork(shortName + "-network", {
     resourceGroupName: resourceGroup.name,
     location: resourceGroup.location,
-    addressSpaces: ["10.0.0.0/16"]
+    addressSpaces: ["10.0.0.0/16"],
 });
 
 const subnet = new azure.network.Subnet(shortName + "-subnet", {
     resourceGroupName: resourceGroup.name,
     virtualNetworkName: network.name,
-    addressPrefix: "10.0.2.0/24"
+    addressPrefix: "10.0.2.0/24",
 });
 
-const publicIP = new azure.network.PublicIp(shortName + "-ip", {
+const publicIPResource = new azure.network.PublicIp(shortName + "-ip", {
     resourceGroupName: resourceGroup.name,
     location: resourceGroup.location,
-    publicIpAddressAllocation: "Dynamic"
+    publicIpAddressAllocation: "Dynamic",
 });
 
 const networkInterface = new azure.network.NetworkInterface(shortName + "-nic", {
@@ -41,18 +38,18 @@ const networkInterface = new azure.network.NetworkInterface(shortName + "-nic", 
         name: "webserveripcfg",
         subnetId: subnet.id,
         privateIpAddressAllocation: "Dynamic",
-        publicIpAddressId: publicIP.id
-    }]
-})
+        publicIpAddressId: publicIPResource.id,
+    }],
+});
 // end network setup -----------------------------------------------------------
 
 const userData =
-`#!bin/bash
+    `#!bin/bash
 echo "Hello World!" > index.html
 nhup python -m SimpleHTTPServer 80 &`;
 
 // create the virtual machine --------------------------------------------------
-let vm = new azure.compute.VirtualMachine(shortName + "-vm", {
+const vm = new azure.compute.VirtualMachine(shortName + "-vm", {
     resourceGroupName: resourceGroup.name,
     location: resourceGroup.location,
     networkInterfaceIds: [networkInterface.id],
@@ -80,11 +77,10 @@ let vm = new azure.compute.VirtualMachine(shortName + "-vm", {
     },
 });
 
-// The public IP address it not allocated until VM is running, resolve
-//  promise (wait for it) to get it.
-exports.publicIP = vm.id.apply(async vm => 
-    await azure.network.getPublicIP({
-        name: publicIP.name,
-        resourceGroupName: resourceGroup.name,
-    }).then(ip => ip.ipAddress)
-);
+export const publicIP = pulumi.all([vm.name, publicIPResource.name]).apply(async ([v, i]) => {
+    const ipResource = await azure.network.getPublicIP({
+        name: i,
+        resourceGroupName: v,
+    });
+    return ipResource.ipAddress;
+});
